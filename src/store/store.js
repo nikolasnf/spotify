57bf05a9f2ef042d38508f0f0ba9c282de4581df
root <rodrigo@astl.com.br>
2021-08-08 02:49:18 +0000

    Adição de estrutura de tracks

--- src/store/store.js
+++ src/store/store.js
@@ -3,13 +3,15 @@ import thunk from 'redux-thunk';
 import albumsReducer from '../reducers/albums';
 import artistsReducer from '../reducers/artists';
 import playlistReducer from '../reducers/playlist';
+import tracksReducer from '../reducers/tracks';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
   combineReducers({
     albums: albumsReducer,
     artists: artistsReducer,
-    playlist: playlistReducer
+    playlist: playlistReducer,
+    tracks: tracksReducer
   }),
   composeEnhancers(applyMiddleware(thunk))
 );
