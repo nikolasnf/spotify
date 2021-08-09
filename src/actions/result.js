b82ec1814428961ceb8a294814c32dded01bed57
root <rodrigo@astl.com.br>
2021-08-09 00:40:17 +0000

    Resolução de album

--- src/actions/result.js
+++ src/actions/result.js
@@ -104,19 +104,13 @@ export const initiateLoadMorePlaylist = (url) => {
 export const initiateTrack = (id) => {
   return async (dispatch) => {
     try {
-      const API_URL = `https://api.spotify.com/v1/tracks/${encodeURIComponent(
+      const API_URL = `https://api.spotify.com/v1/albums/${encodeURIComponent(
         id
       )}`;
-console.log(API_URL)
       const result = await get(API_URL);
-      console.log(result);
-      const { albums, artists, playlists } = result;
-      dispatch(setAlbums(albums));
-      dispatch(setArtists(artists));
-      return dispatch(setPlayList(playlists));
+      return result;
     } catch (error) {
       console.log('error', error);
     }
-  };
-
+  }
 };
--- src/components/Album.js
+++ src/components/Album.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Card } from 'react-bootstrap';
 import _ from 'lodash';
 import music from '../images/music.jpeg';
@@ -7,33 +7,35 @@ import {
   initiateTrack
 } from '../actions/result';
 
+
 const Album = (props) => {
-  const track = initiateTrack(props.match.params.id)
-  return (
+  const album = initiateTrack(props.match.params.id);
+  return ( 
     <React.Fragment className="content">
     <Left />
     <div className='col-right album'>
-      <p className='back'>
-        <a href='/'>
-          <b>&laquo;</b>Voltar
-        </a>
-      </p>
-      <div className='disc col-left'>
-        <div className='box'></div>
-        <p className='album'>Nome do &aacute;lbum</p>
-        <p className='artist'>Nome do artista</p>
-      </div>
-      <div className='col-right'>
-        <ol>
-          <li className='track'>
-            <a href='#'>Nome da Faixa</a>
-            <abbr>3:33</abbr>
-          </li>
-        </ol>
-      </div>
+        <p className='back'>
+          <a href='/'>
+            <b>&laquo;</b>Voltar
+          </a>
+        </p>
+        <div className='disc col-left'>
+          <div className='box'><img src={album.image}/></div>
+ 
+         <p className='album'>Nome do &aacute;lbum</p>
+          <p className='artist'>Nome do artista</p>
+        </div>
+        <div className='col-right'>
+          <ol>
+            <li className='track'>
+              <a href='#'>Nome da Faixa</a>
+              <abbr>3:33</abbr>
+            </li>
+          </ol>
+        </div>
     </div>
     </React.Fragment>
-  );
+    )
 };
 
 export default Album;
