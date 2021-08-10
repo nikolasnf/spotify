e41c57dc27ecd4111d37bc964c95f1cf292be089
root <rodrigo@astl.com.br>
2021-08-10 17:37:44 +0000

    Ajustes finais

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,6 +4,7 @@ import { connect } from 'react-redux';
 import { Button } from 'react-bootstrap';
 import Header from './Header';
 import { Redirect } from 'react-router-dom';
+import { SpotifyAuth } from 'react-spotify-auth'
 
 const Home = (props) => {
   const {
@@ -11,9 +12,16 @@ const Home = (props) => {
     REACT_APP_AUTHORIZE_URL,
     REACT_APP_REDIRECT_URL
   } = process.env;
+  const scopes = [
+  'user-read-currently-playing',
+  'user-read-recently-played',
+  'user-read-playback-state',
+  'user-top-read',
+  'user-modify-playback-state',
+  ]
 
   const handleLogin = () => {
-    window.location = `${REACT_APP_AUTHORIZE_URL}?client_id=${REACT_APP_CLIENT_ID}&redirect_uri=${REACT_APP_REDIRECT_URL}&response_type=token&show_dialog=true`;
+    window.location = `${REACT_APP_AUTHORIZE_URL}?client_id=${REACT_APP_CLIENT_ID}&redirect_uri=${REACT_APP_REDIRECT_URL}&scopes=${scopes}&response_type=token&show_dialog=true`;
   };
 
   const { isValidSession, location } = props;
@@ -31,9 +39,12 @@ const Home = (props) => {
           {sessionExpired && (
             <Alert variant="warning">Sessão Expirada. Logar novamente</Alert>
           )}
-          <Button variant="success" type="submit" onClick={handleLogin}>
-            Logar com spotify
-          </Button>
+          <SpotifyAuth
+            redirectUri='http://spotify.luby.com.br/redirect'
+            clientID='6a4e0ca92464470b9de6cbc0b947a240'
+            clientSecret='43b9b5edc7764fd59c9db9d6b52a1bfe'
+            scopes={scopes}
+          />
         </div>
       )}
     </React.Fragment>
